86948c107ab918312772435db60b29f17cde790b
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-04-26 08:56:31 -0500

    Project setup

---
+++ imports/ui/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import {Meteor} from 'meteor/meteor';
+import {withTracker} from "meteor/react-meteor-data";
+
+import "./App.css";
+
+class App extends Component {
+
+}
+
+export default withTracker(() => {
+    /*Meteor.subscribe("appusers");
+    Meteor.subscribe("sorteos");
+    let all = TossUps.find().fetch();
+    all.sort((a,b)=>b.createdAt-a.createdAt);
+    let users = Users.find().fetch();
+    return {
+        currentUser: Meteor.user(),
+        users: users,
+        sorteos: all
+    }*/
+})(App);
